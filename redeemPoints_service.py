d3c3ccfdf73a144d5ef524a16901477c1f42727c
isaacelroi <isaac@elroi.io>
2026-05-12 21:10:48 +0530

    update discount API URL in redeem_points_service function

--- redeemPoints_service.py
+++ redeemPoints_service.py
@@ -74,7 +74,7 @@ def redeem_points_service(shop, customer_id):
                 }
         
         try:
-            url = "https://shopify.elroi.io/api/create-discount"
+            url = "https://testshopify.readyalliance.io/api/create-discount"
             token = os.getenv("SHOPIFY_API_KEY")
             payload = {
                 "shop": shop,
